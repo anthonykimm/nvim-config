f98f5a5adece0f1fc4c68faca3e4d8288980a722
Anthony Kim <akim0023@student.monash.edu>
2025-11-10 10:49:56 +1100

    feat: add prettier formatting for tsx/jsx

--- lua/plugins/formatting.lua
+++ lua/plugins/formatting.lua
@@ -30,7 +30,9 @@ return {
 			formatters_by_ft = {
 				lua = { "stylua" },
 				javascript = { "prettier" },
+				javascriptreact = { "prettier" },
 				typescript = { "prettier" },
+				typescriptreact = { "prettier" },
 			},
 		},
 	},
